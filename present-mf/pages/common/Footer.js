1a7765a605aa2cfafdd89701f7bcbb02021aa964
thiyagu <thiyagu5334raj@gmail.com>
2022-11-12 00:49:40 +0530

    Added footer routing

--- present-mf/pages/common/Footer.js
+++ present-mf/pages/common/Footer.js
@@ -1,13 +1,27 @@
-import Link from 'next/link';
 import React from 'react';
 import { ArrowRight, ArrowLeft } from "react-feather"
 import { linkItems } from './Header';
+import { useRouter } from 'next/router';
 
 function Footer(props) {
+
+    const router = useRouter()
+
+    const getRoute = isNext => {
+        if (linkItems?.length && router?.pathname) {
+            const currentPage = linkItems.findIndex(item => item?.url === router.pathname)
+            const lastIndex = linkItems.length - 1
+            if(isNext) {
+               return linkItems[currentPage < lastIndex ? currentPage + 1 : 0]?.url
+            }
+           return linkItems[currentPage > 0 ? currentPage - 1 : lastIndex]?.url
+        }
+       return linkItems[0]?.url
+    }
     return (
         <div className=' flex items-center gap-10 justify-center text-center p-10 py-1 border-t border-gray-200 text-gray-400 sticky bottom-0 bg-white text-xs font-semibold'>
-            <Link href="/"><ArrowLeft className="h-8 w-10 text-white bg-gray-600 hover:bg-gray-400 border rounded-full p-1 cursor-pointer" /></Link>
-            <Link href="/nbnbnb"><ArrowRight className="h-8 w-10 text-white bg-gray-600 hover:bg-gray-400 border rounded-full p-1 cursor-pointer" /></Link>
+            <ArrowLeft onClick={() => router.push(getRoute(false))} className="h-8 w-10 text-white bg-gray-600 hover:bg-gray-400 border rounded-full p-1 cursor-pointer" />
+            <ArrowRight onClick={() => router.push(getRoute(true))} className="h-8 w-10 text-white bg-gray-600 hover:bg-gray-400 border rounded-full p-1 cursor-pointer" />
         </div>
     );
 }
--- present-mf/pages/common/Header.js
+++ present-mf/pages/common/Header.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React, { useState } from 'react';
 import { Menu } from "react-feather"
 import { ObSlider } from '../../ObUik';
@@ -30,7 +29,7 @@ export const linkItems = [
 function Header(props) {
 
     const [showLink, setShowLink] = useState(false)
-    const getUiLink = (url, name) => <Link href={url}><li className={`py-3 cursor-pointer text-white w-full pl-12 hover:bg-white hover:text-gray-500 ${router.pathname === url ? "text-gray-300" : ""}`}>{name}</li></Link>
+    const getUiLink = (url, name, index) => <li onClick={() => router.push(url)} key={index} className={`py-3 cursor-pointer text-white w-full pl-12 hover:bg-white hover:text-gray-500 ${router.pathname === url ? "text-gray-300" : ""}`}>{name}</li>
     return (
         <div className='p-10 py-6 border-b border-gray-200 sticky top-0 bg-white z-100'>
             <Menu onClick={() => setShowLink(true)} className="h-10 w-10 text-gray-500 cursor-pointer" />
@@ -39,7 +38,7 @@ function Header(props) {
                     <ObSlider.Body className=" bg-gray-400 pt-10">
                         <ul className='flex flex-col items-center justify-center'>
                             {
-                                linkItems.map(item => getUiLink(item.url, item.name))
+                                linkItems.map((item, index) => getUiLink(item.url, item.name, index))
                             }
                         </ul>
                     </ObSlider.Body>
