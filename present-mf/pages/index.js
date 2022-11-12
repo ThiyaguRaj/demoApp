96b2bd62b5aaf7ce19ec4f3b76932dd1c7e53e9d
thiyagu <thiyagu5334raj@gmail.com>
2022-11-13 00:38:26 +0530

    Changed escape characters

--- present-mf/pages/index.js
+++ present-mf/pages/index.js
@@ -7,21 +7,20 @@ export default function Home() {
     <div className="flex flex-col lg:flex-row items-center justify-center lg:mt-[15vh] lg:gap-10 gap-20">
       <img
         width="450"
-        src="	https://react-intro-tb.netlify.app/static/media/web.b0ad84dd813c7b932937.gif"
+        src="https://react-intro-tb.netlify.app/static/media/web.b0ad84dd813c7b932937.gif"
       />
       <div className="text-center">
         <div className="text-5xl font-bold text-black mb-3">
           Reusable components!
         </div>
         <span style={{ lineHeight: "1px" }}>
-          We're going to talk about what micro frontend is, how we can adopt
-          reusable components and how we can acheive this in our applications.{" "}
+          We`re going to talk about what micro frontend is, how we can adopt
+          reusable components and how we can acheive this in our applications.
         </span>
         <button
           className="cssbuttons-io-button mx-auto mt-4"
           onClick={() => router.push("/components/Introduction")}
         >
-          {" "}
           Get started
           <div className="icon">
             <svg
